10359bd06397cacdcf2eed182a1e187963073ed0
CraigSmith789 <craig789@gmail.com>
2021-02-14 21:22:24 -0500

    seed DB and controller

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -1,2 +1,7 @@
 class PairingsController < ApplicationController
+
+  def index
+    pairings = Pairing.all
+    render json: pairings.to_json(except: [:created_at, :updated_at, :id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pairing.create([
+
+{name: 'Two Beer', style: 'Lager', brewery: 'Left Nut', abv: 4.3, food: 'sweet, hot or spicy foods'},
+{name: 'Three Beer', style: 'Pale Ale', brewery: 'Crazy Brews', abv: 5.7, food: 'acidic and spicey flavors'},
+{name: 'Four Beer', style: 'Stout', brewery: 'Goose Island', abv: 6.2, food: 'spicy, heat-charred'},
+{name: 'Five Beer', style: 'Pilsner', brewery: 'Basement Brews', abv: 5.5, food: 'salty and fried foods'},
+{name: 'Six Beer', style: 'lager', brewery: 'Miller Brewing', abv: 4.8, food: 'rich, spicy and smoked foods'}
+])
